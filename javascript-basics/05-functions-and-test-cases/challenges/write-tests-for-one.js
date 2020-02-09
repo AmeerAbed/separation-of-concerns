d5f686d85ea6667da56b208478dbe20435fdbd68
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-02-09 13:38:16 +0100

    write-tests-for-one

--- javascript-basics/05-functions-and-test-cases/challenges/write-tests-for-one.js
+++ javascript-basics/05-functions-and-test-cases/challenges/write-tests-for-one.js
@@ -5,9 +5,9 @@ const testCases = [
   { name: 'second', args: [], expected: '(true) 1, a (false)' },
   { name: 'third', args: [], expected: '(false) null, null (false)' },
   { name: 'fourth', args: [], expected: '(false) NaN, NaN (false)' },
-  { name: 'fifth', args: [NaN, NaN], expected: _ },
-  { name: 'sixth', args: [], expected: _ },
-  { name: 'seventh', args: [], expected: _ },
+  { name: 'fifth', args: [NaN, NaN], expected: '' },
+  { name: 'sixth', args: [], expected: '' },
+  { name: 'seventh', args: [], expected: '' },
 
   // for the last two, find different arguments with the same expected value
   { name: 'seventh', args: [], expected: '(true) 1, 1 (true)' },
@@ -19,8 +19,10 @@ function typo(x, y) {
   const typeofY = typeof y;
   const areEqual = x === y;
   return `(${typeofX === typeofY}) ${x}, ${y} (${areEqual})`;
+  // template literals are simpler to read/write than concatenation
+  // return "(" + (typeofX === typeofY) + ") " + x + ", " + y + " (" + areEqual + ")";
 }
 
 testCases.forEach(function evaluateTestCase(test) {
-  console.assert(typo(...test.args) === test.expected, test.name);
+  console.assert(typo(...test.args) === test.expected, test.name + ' ' + typo(...test.args));
 });
